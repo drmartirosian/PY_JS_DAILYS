8c6425e85bff90307b1f9fdc0fa3a6e1bc115aff
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-03-01 17:52:55 -0600

    Initial refresher code

--- PYTHON100.py
+++ PYTHON100.py
@@ -1,3 +1,30 @@
 #PYTHON100
 
-#TEST
+#---CONSOLE PRINT
+# print("Hello world!")
+
+#---QUOTES WITHIN QUOTES: "" and ''
+# print("print('What to print?')")
+
+#---BREAK LINE \n
+# print("TEST \n TEST")
+
+#---COMBINING STRINGS
+# print("Hello"+" World!")
+# print("Hello"+" "+"World!")
+
+#---INPUT
+# input("WHAT IS YOUR NAME?")
+
+# name = (input("What is yur name...? "))
+# print(name)
+
+# year = int(input("YEAR?"))
+# print(year)
+
+#---INPUT, THEN RESPOND
+# print msg incomplete, waits for input get rest of msg
+# print("Hello, " + input("What is your name? ") + "!")
+
+#---CHALLENGE: PRINT # WORDS IN NAME
+print("NAME LENGTH:", len(input("NAME? ")))
