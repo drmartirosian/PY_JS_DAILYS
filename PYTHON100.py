66b80fdde62d1ae15a27b0af2cdea70f0887e612
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-03-01 18:34:32 -0600

    day 1 done

--- PYTHON100.py
+++ PYTHON100.py
@@ -1,20 +1,20 @@
 #PYTHON100
 
-#---CONSOLE PRINT
+# -----------------PRINT------------------------#
 # print("Hello world!")
 
-#---QUOTES WITHIN QUOTES: "" and ''
-# print("print('What to print?')")
+# -----------------QUOTES WITHIN QUOTES---------#
+#print("print('What to print?')")
 
-#---BREAK LINE \n
+# -----------------LINE BREAK-------------------#
 # print("TEST \n TEST")
 
-#---COMBINING STRINGS
+# -----------------CONCAT-----------------------#
 # print("Hello"+" World!")
 # print("Hello"+" "+"World!")
 
-#---INPUT
-# input("WHAT IS YOUR NAME?")
+# -----------------INPUT------------------------#
+#input("WHAT IS YOUR NAME?")
 
 # name = (input("What is yur name...? "))
 # print(name)
@@ -22,9 +22,51 @@
 # year = int(input("YEAR?"))
 # print(year)
 
-#---INPUT, THEN RESPOND
+# -----------------INPUT, RESPONSE---------------#
 # print msg incomplete, waits for input get rest of msg
 # print("Hello, " + input("What is your name? ") + "!")
 
-#---CHALLENGE: PRINT # WORDS IN NAME
-print("NAME LENGTH:", len(input("NAME? ")))
+# -----------------LENGTH------------------------#
+# print("NAME LENGTH:", len( input("NAME? ")))
+# or...
+# name = (input("What is your name? "))
+# print(len(name))
+
+# -----------------VARIABLES---------------------#
+# name = "Jack" #original...
+# name = "Bob" #changed to...
+# name = input("WHAT IS YOUR NAME? ") #now is input value
+# print(name)
+
+# CHALLENGE: SWITCH VALUES...
+# a = input("a:")
+# b = input("b:")
+
+# c = a
+# a = b
+# b = c
+# or...
+# a = [a, b]
+# b = a[0]
+# a = a[1]
+# or...
+# a = [input("a:"), input("b:")]
+# b = a[0]
+# a = a[1]
+
+# print ("a:"+a+"\nb:"+b )
+
+# -----------------NAMING---------------------#
+# Snake case...
+# no numbers at start of name...
+# No caps...
+
+user_name = "name"
+
+# --------------"ONLINE" NAME GENERATOR-----------------#
+print("Welcome to band generator!")
+pet = (input("What is your pet? "))
+city = (input("What is your home street? "))
+print("Your 'online' name could be "+pet+" "+city+"!")
+
+#=====================DAY-2=============================#
