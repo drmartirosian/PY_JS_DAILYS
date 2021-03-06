440afde34448a5c392975b32a9e12fd2e8993961
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-03-05 23:48:37 -0600

    task-thread counter

--- TEST.py
+++ TEST.py
@@ -1,7 +1,36 @@
-secret = ['a','b','c']
+tasks = [3,1,2,7]
+threads = 2
+counter = 0
 
-current = secret[secret.index('b')]
+def my_computer():
+    global counter, tasks
+    # print(tasks)
+    while len(tasks) > 0:
+        perform_tasks()
+        remove_finished()
+        # print(f'{tasks}=>{counter}')
+    print(f"Tasks complete: {counter}")
 
-secret.remove(current)
+def perform_tasks():
+    global counter, tasks
+    if len(tasks) > 1:
+        counter += 1
+        for task in range(threads):
+            tasks[task] -= 1
+    elif len(tasks) == 1:
+        counter += 1
+        for task in range(1):
+            tasks[task] -= 1
 
-print(secret)
+def remove_finished():
+    global counter, tasks
+    while 0 in tasks:
+        for task in tasks:
+            if task <= 0:
+                tasks.remove(0)
+
+my_computer()
+
+
+
+
