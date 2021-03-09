475f1ade0457a6abbf3e57740106b92fd8e0798a
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-03-08 18:03:33 -0600

    BLACK JACK start

---
+++ PYTHON_PROJ/BLACK_JACK.py
@@ -0,0 +1,13 @@
+#ASK FOR CARDS
+#RANDOM CARDS FROM 52
+#CAN"T GO OVER 21 - BUST!
+#JACK, KING, QUEEN = 1-
+#ACE = 1 or 11
+# DEALER SHOWS 1 card, rest of hand hidden
+#Can see own hand
+#HIT, STAY
+#DRAWS
+#DEALER HAND < 17, must take another card
+# 1. do you want to play? y/n
+# ace = 11 untl over 21, then 1
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
