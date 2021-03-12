b05746bf7139aafa248fb236bf799bcaa2a6169a
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-03-11 20:33:03 -0600

    DAY16 continued...

--- PYTHON_PROJ/day-16-start/main.py
+++ PYTHON_PROJ/day-16-start/main.py
@@ -9,18 +9,23 @@
 
 
 
-import turtle
-test = turtle.Turtle()
-#POLYGON
-for i in range(6):
-    test.forward(100) #move forward 100
-    test.right(300) #turn at 300 angle
-#SQUARE
+# import turtle
+# test = turtle.Turtle()
+# #POLYGON
+# test.shape('turtle')
+# test.color('red')
+# for i in range(6):
+#     test.forward(100) #move forward 100
+#     test.right(300) #turn at 300 angle
+# #SQUARE
+# test.color('green')
 # for i in range(4):
 #     test.forward(100)
 #     test.left(90)
-#CIRCLE
+# #CIRCLE
+# test.color('blue')
 # for i in range(13):
 #     test.forward(10)
 #     test.right(30)
-turtle.done()
+# turtle.done()
+
