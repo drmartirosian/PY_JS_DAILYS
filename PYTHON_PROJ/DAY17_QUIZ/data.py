edfbe102594defe96d01901c09bb0123d5117d98
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-03-16 21:44:33 -0500

    DAY18 - Dot Proj start

--- PYTHON_PROJ/DAY17_QUIZ/data.py
+++ PYTHON_PROJ/DAY17_QUIZ/data.py
@@ -12,3 +12,6 @@ question_data = [
     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
 ]
+
+#More questions...
+# https://opentdb.com/api_config.php
---
+++ PYTHON_PROJ/DAY18_Dots/main.py
@@ -0,0 +1,18 @@
+# from turtle import * #IMPORT EVERYTHING
+from turtle import Turtle,Screen
+#==============================START====================================#
+timmy = Turtle() #Generate turtle object "timmy"
+
+# timmy.shape("turtle")
+# timmy.color('red')
+# timmy.dot(100,'blue')
+
+# for i in range(4):
+#     timmy.forward(100)
+#     timmy.left(90)
+
+
+#================================END=====================================#
+my_screen = Screen() # Show in screen, 
+my_screen.exitonclick() # exist ONLY on click
+
