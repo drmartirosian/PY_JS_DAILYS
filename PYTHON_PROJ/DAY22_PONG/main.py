881452cc4b350f8f46fc5a0c201b9a29d734848c
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-04-01 23:21:22 -0500

    clear up

--- PYTHON_PROJ/DAY22_PONG/main.py
+++ PYTHON_PROJ/DAY22_PONG/main.py
@@ -39,7 +39,7 @@ while game_is_on == True:
     #Detect collision with paddles
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
-    #IF BALL GOES RIGHT...
+    #IF BALL GOES RIGHT... OR LEFT
     if ball.xcor() > 380: 
         ball.reset_position()
         scoreboard.l_score += 1
