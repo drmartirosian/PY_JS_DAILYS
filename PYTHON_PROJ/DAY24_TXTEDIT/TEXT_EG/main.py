a7233ec9b4e79661a13f3d853cc32080039c7901
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-04-12 19:10:08 -0500

    text editor

---
+++ PYTHON_PROJ/DAY24_TXTEDIT/TEXT_EG/main.py
@@ -0,0 +1,14 @@
+# file = open ('data.txt') #open file for use
+# contents = file.read() #read out contents of txt file
+# print(contents)
+# file.close() #close to stop using up CPU resources 
+
+#OR...
+# with open ('data.txt',mode='a') as file: #mode=(r)ead,(w)rite,(a)ppend
+#     # contents = file.read() #read out contents of txt file
+#     # print(contents)
+#     file.write('\nThe cake is a lie!')
+
+#GENERAT ENEW TXT FILES
+with open('new_file.txt',mode='w') as file:
+    file.write("\nThe cake is a lie.")
