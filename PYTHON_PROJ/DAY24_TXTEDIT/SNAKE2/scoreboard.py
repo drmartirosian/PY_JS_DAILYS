3d312ad494951631871168f928321d8de37d0d9a
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-04-12 20:17:55 -0500

    data.txt file updates high score

--- PYTHON_PROJ/DAY24_TXTEDIT/SNAKE2/scoreboard.py
+++ PYTHON_PROJ/DAY24_TXTEDIT/SNAKE2/scoreboard.py
@@ -8,7 +8,11 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+
+        #READ HIGH_SCORE FROM DATA.TXT FILE
+        with open('data.txt', mode='r') as data:
+            self.high_score = int(data.read())
+
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,6 +26,10 @@ class Scoreboard(Turtle):
     def reset(self): #replaces game over, resets scoreboard
         if self.score > self.high_score:
             self.high_score = self.score
+
+            with open('data.txt',mode='w') as data:
+                data.write(f"{self.high_score}")
+
         self.score = 0
         self.update_scoreboard()
 
